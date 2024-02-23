26fb7ea551827109823dae2f668d40b9cbee3fa8
leonardonunes3 <leonardo.nunes793@gmail.com>
2024-02-23 01:23:09 -0300

    sign up page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,16 +1,45 @@
-import { Container } from './styles';
-import { Button } from '../../components/Button';
-import { Header } from '../../components/Header';
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
+
+import { Container, Form } from "./styles";
+
+import logo  from "../../assets/Logo.svg";
 
 export function SignUp() {
 
   return(
+      <Container>
+        <Form>
+          <h1>Crie sua conta</h1>
 
-    <Container>
-      <Header />
-      Hello World!
-      <Button title="Criar conta" />
-    </Container>
-    
+          <p>Seu nome</p>
+          <Input 
+            placeholder="Exemplo: Maria da Silva"
+            alternativeInput
+          />
+          
+          <p>Email</p>
+          <Input 
+            placeholder="Exemplo: exemplo@exemplo.com.br"
+            alternativeInput
+          />
+
+          <p>Senha</p>
+          <Input 
+            placeholder="No mínimo 6 caracteres"
+            type="password"
+            alternativeInput
+          />
+
+          <Button title="Criar uma conta" />
+
+          <ButtonText title="Já tenho uma conta" />
+          
+        </Form>
+
+        <img src={logo} />
+      </Container>
+
   )
 }
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,7 +1,57 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
     height: 100vh;
+
+    display: flex;
+    flex-direction: row-reverse;
+
+    img {
+        
+        height: 48px;
+        width: 324px;
+
+        margin-top: 340px;
+        margin-right: 306px;
+    }
+
+`;
+
+export const Form = styled.form`
+    width: 476px;
+    min-height: 620px;
+    border-radius: 16px;
+
+    margin: 90px 108px 112px 0;
+    padding: 0 64px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    text-align: center;
+    font-size: 14px;
+
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_DARK_700 };
+
+    h1 {
+        font-size: 32px;
+        font-weight: 400;
+        margin-top: 64px;
+        margin-bottom: 32px;
+    }
+
+    p {
+        display: flex;
+        align-self: start;
+        margin-bottom: 8px;
+        font-family: "Roboto", sans-serif;
+        font-size: 16px;
+        color: ${({ theme }) => theme.COLORS.LIGHT_400 };
+    }
+
+    button {
+        margin-bottom: 32px;
+    }
 
 `;
