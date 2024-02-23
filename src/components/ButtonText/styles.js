1452d6412f5ff0b8de5b4bf6d740396734324737
leonardonunes3 <leonardo.nunes793@gmail.com>
2024-02-23 01:21:16 -0300

    button text

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+
+    background: none;
+    font-size: 14px;
+    color: ${({theme}) => theme.COLORS.WHITE };
+    border: none;
+`;
